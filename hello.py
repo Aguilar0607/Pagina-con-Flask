abd7bf81580ff2324e6594342b686ec56a48e3a3
Aguilar0607 <emmanuelaguilar0607@gmail.com>
2022-11-15 16:02:49 +0000

    Pagina Web con Flask haciendo uso de la API

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -33,8 +33,26 @@ def rick_and_morty(page=1):
     
 @app.route("/search", methods=['GET', 'POST'])
 def search():
-    print(requests)
-    return "Busqueda"
+    search =request.form['search']
+    if len(search) > 0:
+        url = f'https://rickandmortyapi.com/api/character/?name={search}'
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        respuesta_json = response.json()
+        info = respuesta_json['info']
+        personajes = respuesta_json['results']
+        if info['pages'] > 1:
+            next = 2
+            prev = 1
+        else:
+            next = 1
+            prev = 1
+                    
+        return render_template('rick.html', personajes=personajes, prev=prev, next=next)
+
+    else:
+        return 'No se puede hacer la busqueda'
 
 if __name__ == '__main__':
     app.run(debug=True)
